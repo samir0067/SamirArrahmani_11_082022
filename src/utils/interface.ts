64e2c3aeff81cbb18176786f21ce3c1fc35eaa68
samir0067 <arrahmani.samir@gmail.com>
2022-09-07 15:47:46 +0200

    wip interface

---
+++ src/utils/interface.ts
@@ -0,0 +1,15 @@
+export interface ApartmentDataProps {
+  id?: string;
+  title: string;
+  cover: string;
+  pictures: string[];
+  description: string;
+  host: {
+    name: string;
+    picture: string;
+  };
+  rating: number;
+  location: string;
+  equipments: string[];
+  tags: string[];
+}
