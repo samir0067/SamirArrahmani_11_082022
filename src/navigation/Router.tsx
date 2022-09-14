d980fd732e1aab35f3bc803473f92211a0e7312c
samir0067 <arrahmani.samir@gmail.com>
2022-09-14 11:56:28 +0200

    wip style

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -3,6 +3,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "../pages/Home";
 import AboutUs from "../pages/AboutUs";
 import NotFound from "../pages/NotFound";
+import Apartment from "../pages/Apartment";
 
 function Router() {
   const location = useLocation();
@@ -10,6 +11,7 @@ function Router() {
   return (
     <Routes key={location.pathname} location={location}>
       <Route path="/" element={<Home />} />
+      <Route path="/appartement/:appartementId" element={<Apartment />} />
       <Route path="/a-propos-de-nous" element={<AboutUs />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,40 @@
-import React, { FC } from "react";
+import React, { FC, Fragment } from "react";
 import { NavBar } from "../layout/NavBar";
 import { Footer } from "../layout/Footer";
-import { Header } from "../components/Header";
+import { Header } from "../layout/Header";
 import imageBackHeadHome from "../assets/imageBackHeadHome.png";
 import "./home.css";
+import { ApartmentCard } from "../components/ApartmentCard";
+import { Link } from "react-router-dom";
+import { useFetchAll } from "../utils/hooks/useFetchAll";
 
 const Home: FC = () => {
+  const { allData, isLoading, error } = useFetchAll(`/data.json`);
+
+  if (error) {
+    return <h2 className="text-center">Oups!! il y a eu un problème</h2>;
+  }
+
   return (
-    <div className="containerHome">
+    <Fragment>
       <NavBar />
-      <Header title="Chez vous, partout et ailleurs" image={<img src={imageBackHeadHome} alt="home landscape" />} />
-      <Footer />
-    </div>
+      <Header title="Chez vous, partout et ailleurs" image={<img src={imageBackHeadHome} alt="paysage urbain" />} />
+      <main className="containerHome">
+        <section>
+          <ul className="apartmentContainer">
+            {isLoading && <h1>Chargement en cour</h1>}
+            {allData.map((apartment, index) => (
+              <li key={apartment.id}>
+                <Link to={`/appartement/${apartment.id}`}>
+                  <ApartmentCard picture={apartment.cover} title={apartment.title} id={index} />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <Footer />
+      </main>
+    </Fragment>
   );
 };
 
