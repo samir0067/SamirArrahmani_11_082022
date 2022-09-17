c681c5120ca953a5dac56e06531b71f2d6c1bef1
samir0067 <arrahmani.samir@gmail.com>
2022-09-17 11:35:57 +0200

    implementation du carousel de du useFetchById

--- src/pages/Apartment.tsx
+++ src/pages/Apartment.tsx
@@ -1,24 +1,37 @@
-import React, { FC, Fragment } from "react";
-import { NavBar } from "../layout/NavBar";
-import { Footer } from "../layout/Footer";
-import "./aboutUs.css";
-import { Navigate, useParams } from "react-router-dom";
+import React, { FC, Fragment, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { useFetchById } from "../utils/hooks/useFetchById";
+import { Footer } from "../layout/Footer";
+import { NavBar } from "../layout/NavBar";
+import { Carousel } from "../components/Carousel";
+import { Header } from "../layout/Header";
+import { useMatchMedia } from "../utils/hooks/useMatchMedia";
+import "./apartment.css";
 
 const Apartment: FC = () => {
+  const navigate = useNavigate();
+  const { downSm } = useMatchMedia();
   const { appartementId } = useParams<string>();
-  const { data, isLoading, error } = useFetchById(`/data.json`, appartementId);
-  console.log("appartementId ==>", appartementId);
-  console.log("data ==>", data);
+  const { data, isLoading, error } = useFetchById(`/data.json`, appartementId!);
 
-  if (!data) {
-    return <Navigate to="/*" replace />;
+  useEffect(() => {
+    if (!isLoading) {
+      if (!data) return navigate("/erreur", { replace: true });
+    }
+  }, [isLoading]);
+
+  if (error) {
+    return <h2 className="errorFetch">Oups! un problème dans la récupération des données.</h2>;
   }
 
   return (
     <Fragment>
       <NavBar />
-      <main className="containerHome"></main>
+      <Header
+        className={downSm ? "headerApartmentMobile" : "headerApartmentLaptop"}
+        content={<Carousel pictures={data?.pictures} title={data?.title} />}
+      />
+      <main className="apartmentMain"></main>
       <Footer />
     </Fragment>
   );
